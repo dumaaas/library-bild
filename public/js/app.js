c061b5a2753e74ee3c53fed355bc39a947732b75
dumaaas <markodumnic8@gmail.com>
2021-04-08 22:05:08 +0200

    Added hover, transition, search focus, hamburger menu..

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,25 @@
-$("#hamburger-menu").click(function (e) {
-    e.preventDefault();
-    $("#sidebar").toggleClass("active");
-    $(".sidebar-item").toggleClass("inline hidden");
-});
+// $("#hamburger-menu").click(function (e) {
+//     e.preventDefault();
+//     $("#sidebar").toggleClass("active");
+//     $(".sidebar-item").toggleClass("inline hidden");
+// });
+
+//open and close hamburger menu
+  $(function () {
+    var hamburger = $('#hamburger');
+    var sidebar = $('.sidebar');
+    hamburger.on('click', function () {
+      if (sidebar.hasClass('active')) {
+        sidebar.removeClass('active');
+        hamburger.removeClass('change');
+        $(".sidebar-item").addClass("hidden");
+        $(".sidebar-item").removeClass("inline");
+      } else {
+        sidebar.addClass('active');
+        hamburger.addClass('change');
+        $(".sidebar-item").removeClass("hidden");
+        $(".sidebar-item").addClass("inline");
+
+      }
+    });
+  });
