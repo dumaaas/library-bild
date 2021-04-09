ea186ae6a9875bbfb0b517e12a92682a1e86c2df
dumaaas <markodumnic8@gmail.com>
2021-04-09 02:41:18 +0200

    Made a lot of changes on side bar

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,49 @@
 //open and close hamburger menu
-  $(function () {
-    var hamburger = $('#hamburger');
-    var sidebar = $('.sidebar');
-    hamburger.on('click', function () {
-      if (sidebar.hasClass('active')) {
-        sidebar.removeClass('active');
-        hamburger.removeClass('change');
-        $(".sidebar-item").addClass("hidden");
-        $(".sidebar-item").removeClass("inline");
-      } else {
-        sidebar.addClass('active');
-        hamburger.addClass('change');
-        $(".sidebar-item").removeClass("hidden");
-        $(".sidebar-item").addClass("inline");
-      }
-    });
+$(function () {
+  var hamburger = $('#hamburger');
+  var sidebar = $('.sidebar');
+
+  hamburger.on('click', function () {
+    if (sidebar.hasClass('active')) { // if menu is opened
+      //close menu by removing active class
+      sidebar.removeClass('active');
+      //make hamburger shape change 
+      hamburger.removeClass('change');
+      //hide text and arrow
+      $(".sidebar-item").addClass("hidden");
+      $(".sidebar-item").removeClass("inline");
+      //hide/close all opened submenues
+      $('.aside-item').hide();
+      //change all arrows which are up to down 
+      $('.arrow').removeClass('fa-angle-up');
+      $('.arrow').addClass('fa-angle-down');
+    } else {
+      //open menu
+      sidebar.addClass('active');
+      //make hamburger shape change
+      hamburger.addClass('change');
+      //show text and arrow 
+      $(".sidebar-item").removeClass("hidden");
+      $(".sidebar-item").addClass("inline");
+    }
   });
+});
+
+//open close submenu
+$(function () {
+  var asideArrow = $('.asideArrow');
+  asideArrow.on('click', function () {
+    //find on which arrow is clicked
+    var num = this.id.match(/\d+/)[0];
+    //toggle submenu on click
+    $('#aside-item_' + num).slideToggle();
+    //change arrow on up or down
+    if($('#arrow-collapse_' + num).hasClass('fa-angle-down')) {
+      $('#arrow-collapse_' + num).addClass('fa-angle-up');
+      $('#arrow-collapse_' + num).removeClass('fa-angle-down');
+    } else {
+      $('#arrow-collapse_' + num).addClass('fa-angle-down');
+      $('#arrow-collapse_' + num).removeClass('fa-angle-up');
+    }
+  });
+});
