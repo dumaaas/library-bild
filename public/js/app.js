2d5753743af52810fd010996cbefa482721595cc
dumaaas <markodumnic8@gmail.com>
2021-04-15 18:17:03 +0200

    Added dropdown menus with live search funcionality  on dashboard activity page

--- public/js/app.js
+++ public/js/app.js
@@ -759,3 +759,57 @@ function sortTableDate(row) {
     }
   }
 }
+
+$('#uceniciMenu').on('click', function() {
+
+  $('.uceniciMenu').toggle();
+  $('.bibliotekariMenu').addClass('hidden');
+  $('.knjigeMenu').addClass('hidden');
+  $('.transakcijeMenu').addClass('hidden');
+
+})
+
+$('#bibliotekariMenu').on('click', function() {
+
+  $('.bibliotekariMenu').toggle();
+})
+
+$('#knjigeMenu').on('click', function() {
+
+  $('.knjigeMenu').toggle();
+})
+
+$('#transakcijeMenu').on('click', function() {
+
+  $('.transakcijeMenu').toggle();
+})
+
+function filterFunction(id, dropdown) {
+  var input, filter, ul, li, a, i;
+  console.log(id);
+  console.log(dropdown);
+
+  input = document.getElementById(id);
+  filter = input.value.toUpperCase();
+  div = document.getElementById(dropdown);
+  text = div.getElementsByTagName("p");
+  label = div.getElementsByTagName("label");
+  img = div.getElementsByTagName("img");
+
+  for (i = 0; i < text.length; i++) {
+    txtValue = text[i].textContent || text[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      text[i].style.display = "";
+      label[i].style.display = "";
+      if(img[i] !== undefined) {
+        img[i].style.display = "";
+      }
+    } else {
+      text[i].style.display = "none";
+      label[i].style.display = "none";
+      if(img[i] !== undefined) {
+        img[i].style.display = "none";
+      }
+    }
+  }
+}
