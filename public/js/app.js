224c3bc0273babf0bd7d88ff8c5dcf750a6f2a99
Emilija Pribanovic <emilija.pribanovic@bild-studio.net>
2021-04-15 19:20:10 +0200

    Popravljen upload ikonice

--- public/js/app.js
+++ public/js/app.js
@@ -284,18 +284,9 @@ var loadFileLibrarian = function (event) {
 };
 
 // Category icon upload
-var inputCategory = document.getElementById( 'icon-upload' );
-var categoryInfoArea = document.getElementById( 'icon-output' );
-inputCategory.addEventListener( 'change', showFileName );
-
-function showFileName( event ) {
-  // the change event gives us the input it occurred in 
-  var input = event.srcElement;
-  // the input has an array of files in the `files` property, each one has a name that you can use. We're just using the name here.
-  var fileName = input.files[0].name;
-  // use fileName however fits your app best, i.e. add it into a div
-  categoryInfoArea.textContent = 'File name: ' + fileName;
-}
+$("#icon-upload").change(function(){
+  $("#icon-output").text(this.files[0].name);
+});
 
 function sortTable() {
   var table, rows, switching, i, x, y, shouldSwitch;
