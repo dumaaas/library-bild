0c775e66415e6c05bc39372a8c2d3f7555f094a4
Emilija Pribanovic <emilija.pribanovic@bild-studio.net>
2021-04-19 12:52:59 +0200

    Izmijenjen link

--- public/noviZanr.php
+++ public/noviZanr.php
@@ -48,7 +48,7 @@
                             <nav class="w-full rounded">
                                 <ol class="flex list-reset">
                                     <li>
-                                        <a href="zanrovi.php" class="text-[#2196f3] hover:text-blue-600">
+                                        <a href="settingsZanrovi.php" class="text-[#2196f3] hover:text-blue-600">
                                             Evidencija zanrova
                                         </a>
                                     </li>
