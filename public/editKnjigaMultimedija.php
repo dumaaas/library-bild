8554ac4f8106174f2fab3c0ed6b84132e1ff257a
Emilija Pribanovic <emilija.pribanovic@bild-studio.net>
2021-04-21 03:58:57 +0200

    ispravljen z index na dugmad na editovanje multimedije slike

--- public/editKnjigaMultimedija.php
+++ public/editKnjigaMultimedija.php
@@ -241,7 +241,7 @@
                     </div>
                     <div class="absolute bottom-0 w-full">
                         <div class="flex flex-row">
-                            <div class="inline-block w-full text-white text-right py-[7px] mr-[100px]">
+                            <div class="inline-block w-full text-white text-right py-[7px] mr-[100px] relative z-50">
                                 <button type="button"
                                     class="shadow-lg mr-[15px] w-[150px] focus:outline-none text-sm py-2.5 px-5 transition duration-300 ease-in bg-[#F44336] hover:bg-[#F55549] rounded-[5px]">
                                     Ponisti <i class="fas fa-times ml-[4px]"></i>
